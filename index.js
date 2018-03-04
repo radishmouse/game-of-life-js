e5fed60fbea9f1aba29e7de1048c677fb80f7cbc
Chris Aquino <chris.aquino@gmail.com>
2018-03-04 08:18:09 -0500

    adds conversion of board of cells to number of neighbors, printing

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ The rules:
   
 */
 
+
+
 const SIZE = 42;
+const INTERVAL = 333;
+const LIVE = '@';
+const DEAD = ' ';
+
 const FEW = 2;
 const MANY = 3;
 const PLENTY = 3;
-
-const LIVE = '@';
-const DEAD = ' ';
 
 const isLive = c => c === LIVE;
 const isUnderPopulated = n => n < FEW;
@@ -23,13 +26,28 @@ const isOverPopulated = n => n > MANY;
 const canReproduce = n => n === PLENTY;
 const willContinue = n => !(isUnderPopulated(n)) && !(isOverPopulated(n));
 
+const getRandomInt = (max, min=0) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
 const newRow = () => Array(SIZE).fill(DEAD);
-const newBoard = () => newRow().map(newRow);
+const newBoard = shouldSeed => {
+  let board = newRow().map(newRow);
+  if (shouldSeed) {
+    board = board.map(row => row.map(c => {
+      return getRandomInt(100) < 50 ? LIVE : DEAD
+    }))
+  }
+
+  return board;
+}
 
 const cloneRow = r => r.slice();
 const cloneBoard = b => b.map(cloneRow);
 
-const isWithinBounds = v => v >= 0 && v < (SIZE - 1);
+const isWithinBounds = v => v >= 0 && v < SIZE;
 const areWithinBounds = (x, y) => isWithinBounds(x) && isWithinBounds(y);
 
 const neighborCoordinates = (x, y) => [
@@ -46,11 +64,10 @@ const coordsForBoard = b => b.map(coordsForRow);
 
 const neighborCoordinatesForRow = (r, x=0) => coordsForRow(r, x).map(xyArr => neighborCoordinates(...xyArr));
 const neighborCoordinatesForBoard = b => b.map(neighborCoordinatesForRow);
-
 // console.log(neighborCoordinatesForBoard(newBoard()));
 
 const cellAtCoorinate = (board, x, y) => board[x][y];
-const liveAtCoord = (board, x, y) => isLive(cellAtCoorinate);
+const liveAtCoord = (board, x, y) => isLive(cellAtCoorinate(board, x, y));
 
 const neighborCellsForCoordinateArray = (board, arrayOfNeighborCoors) => {
   return arrayOfNeighborCoors.map(neighborCoordsForCell => {
@@ -59,5 +76,66 @@ const neighborCellsForCoordinateArray = (board, arrayOfNeighborCoors) => {
     })
   })
 };
-
 // console.log(neighborCellsForCoordinateArray(newBoard(), neighborCoordinatesForBoard(newBoard())))
+
+const boardAsNumberOfNeighbors = (board, arrayOfNeighborCoords) => {
+  return neighborCellsForCoordinateArray(board, arrayOfNeighborCoords).map(neighborCellsForRow => {
+    return neighborCellsForRow.map(neighborCellsForCell => {
+      return neighborCellsForCell
+        .filter(isLive)
+        .reduce((total, c) => total + 1, 0)
+    })
+  })
+}
+// console.log(boardAsNumberOfNeighbors(newBoard(), neighborCoordinatesForBoard(newBoard())))
+
+const numberToLiveDead = (number, cell) => {
+  if (isLive(cell)) {
+    if (isUnderPopulated(number)) {
+      return DEAD;
+    } else if (isOverPopulated(number)) {
+      return DEAD;
+    } else if (willContinue(number)) {
+      return LIVE;
+    }
+  } else if (canReproduce(number)){
+    return LIVE;
+  } else {
+    return DEAD;
+  }
+}
+const numberRowAsLiveDeadCells = (rowOfNumbers, rowOfCells) => rowOfNumbers.map((n, i) => numberToLiveDead(n, rowOfCells[i]));
+const numberBoardAsLiveDeadCells = (boardOfNumbers, boardOfCells) => boardOfNumbers.map((r, i) => numberRowAsLiveDeadCells(r, boardOfCells[i]));
+
+const printRow = r => r.join(' ');
+const printBoard = b => console.log(b.map(printRow).join('\n'));
+
+console.clear();
+
+const mainLoop = (board, coords) => {
+  return 
+}
+
+const main = (board) => {
+  const coords = neighborCoordinatesForBoard(board);
+  let neighbors;
+  let generation = 0;
+  setInterval(() => {
+    neighbors = boardAsNumberOfNeighbors(board, coords);
+    board = numberBoardAsLiveDeadCells(neighbors, board);
+    console.clear();
+    printBoard(board);
+    console.log(`Generation ${generation}`);
+    generation++;
+  }, INTERVAL)
+}
+
+main(newBoard(true));
+
+
+// In the game loop, take a board, create a coordinateboard
+// calculate numberOfNeighbors
+// convert this into live or dead cells in a new board
+// pass that into another call to the game loop (or simply return to top of an actual loop)
+// each pass, clear the screen and print the board of live/dead cells
+// continue until there is no change
