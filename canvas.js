849a32f3077e73ba0a0d7689db6c47d0c361d649
Chris Aquino <chris.aquino@gmail.com>
2018-06-16 09:46:26 -0400

    adds html with canvas element, js for basic drawing

--- canvas.js
+++ canvas.js
@@ -1,1 +1,22 @@
-console.log('hey');
+console.log('Loading Game of Life');
+
+const canvas = document.querySelector('#life');
+const ctx = canvas.getContext('2d');
+const {height, width} = canvas;
+
+const CELLS_PER_ROW = 42;
+
+const COLORS = {
+  live: 'rgb(0, 0, 0)',
+  dead: 'rgb(255, 255, 255)'
+};
+
+const CELL_SIZE = width/CELLS_PER_ROW;
+
+function _renderBox (isLive, x, y) {
+  this.fillStyle = isLive ? COLORS.live : COLORS.dead;
+  this.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+}
+
+const renderLive = _renderBox.bind(ctx, true);
+const renderDead = _renderBox.bind(ctx, false);
