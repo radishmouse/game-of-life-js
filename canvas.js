64251023c5da4a30604eca87baed496b88bd81dd
Chris Aquino <chris.aquino@gmail.com>
2018-06-16 09:58:32 -0400

    can draw live cells to game board

--- canvas.js
+++ canvas.js
@@ -4,19 +4,47 @@ const canvas = document.querySelector('#life');
 const ctx = canvas.getContext('2d');
 const {height, width} = canvas;
 
-const CELLS_PER_ROW = 42;
+const GRID_COUNT = 42;
+const CELL_SIZE = width/GRID_COUNT;
 
 const COLORS = {
   live: 'rgb(0, 0, 0)',
   dead: 'rgb(255, 255, 255)'
 };
 
-const CELL_SIZE = width/CELLS_PER_ROW;
-
-function _renderBox (isLive, x, y) {
+// Uses function declaration for binding `this`
+// to the canvas context.
+function _renderBox (isLive, xGrid, yGrid) {
   this.fillStyle = isLive ? COLORS.live : COLORS.dead;
-  this.fillRect(x, y, CELL_SIZE, CELL_SIZE);
+  this.fillRect(xGrid*CELL_SIZE,
+                yGrid*CELL_SIZE,
+                CELL_SIZE,
+                CELL_SIZE);
 }
 
-const renderLive = _renderBox.bind(ctx, true);
-const renderDead = _renderBox.bind(ctx, false);
+//
+const liveAt = _renderBox.bind(ctx, true);
+const deadAt = _renderBox.bind(ctx, false);
+
+let x = 0;
+let y = 0;
+let rafID;
+
+const animate = () => {
+
+
+  liveAt(x, y);
+  x++;
+  if (x == 42) {
+    x = 0;
+    y++
+  };
+
+  if (y == 42) {
+    cancelAnimationFrame(rafID);
+  } else {
+    rafID = requestAnimationFrame(animate);    
+  }
+}
+
+rafID = requestAnimationFrame(animate);
