2dbe53934c6a341208904f24ec8d1577f6f8c321
Chris Aquino <chris.aquino@gmail.com>
2018-06-16 10:53:54 -0400

    adds drawing functions based on true/false values

--- canvas.js
+++ canvas.js
@@ -1,22 +1,34 @@
 console.log('Loading Game of Life');
 
+const {
+  newBoard,
+  neighborCoordinatesForBoard,
+  boardAsNumberOfNeighbors,
+  isLive,
+  isUnderPopulated,
+  isOverPopulated,
+  willContinue,
+  canReproduce,
+  SIZE
+} = life;
+
 const canvas = document.querySelector('#life');
 const ctx = canvas.getContext('2d');
 const {height, width} = canvas;
 
-const GRID_COUNT = 42;
+const GRID_COUNT = SIZE;
 const CELL_SIZE = width/GRID_COUNT;
 
 const COLORS = {
-  live: 'rgb(0, 0, 0)',
+  live: 'rgba(200, 0, 0, 1)',
   dead: 'rgb(255, 255, 255)'
 };
 
 // Uses function declaration for binding `this`
 // to the canvas context.
 function _renderBox (isLive, xGrid, yGrid) {
-  this.fillStyle = isLive ? COLORS.live : COLORS.dead;
-  this.fillRect(xGrid*CELL_SIZE,
+  this.strokeStyle = isLive ? COLORS.live : COLORS.dead;
+  this.strokeRect(xGrid*CELL_SIZE,
                 yGrid*CELL_SIZE,
                 CELL_SIZE,
                 CELL_SIZE);
@@ -26,25 +38,58 @@ function _renderBox (isLive, xGrid, yGrid) {
 const liveAt = _renderBox.bind(ctx, true);
 const deadAt = _renderBox.bind(ctx, false);
 
-let x = 0;
-let y = 0;
-let rafID;
 
-const animate = () => {
-
-
-  liveAt(x, y);
-  x++;
-  if (x == 42) {
-    x = 0;
-    y++
-  };
-
-  if (y == 42) {
-    cancelAnimationFrame(rafID);
+const numberToIsLive = (number, cell) => {
+  if (isLive(cell)) {
+    if (isUnderPopulated(number)) {
+      // return DEAD;
+      return false;
+    } else if (isOverPopulated(number)) {
+      // return DEAD;
+      return false
+    } else if (willContinue(number)) {
+      // return LIVE;
+      return true;
+    }
+  } else if (canReproduce(number)){
+    // return LIVE;
+    return true;
   } else {
-    rafID = requestAnimationFrame(animate);    
+    // return DEAD;
+    return false;
   }
+};
+
+// Given rows or boards of cells and neighbor counts, calculate next states.
+const numberRowAsLiveDeadCells = (rowOfNumbers, rowOfCells) => rowOfNumbers.map((n, i) => numberToIsLive(n, rowOfCells[i]));
+const numberBoardAsLiveDeadCells = (boardOfNumbers, boardOfCells) => boardOfNumbers.map((r, i) => numberRowAsLiveDeadCells(r, boardOfCells[i]));
+
+
+const renderRow = (r, y) => r.map((c, i) => (c ? liveAt(i, y) : deadAt(i, y)) && c);
+const renderBoard = b => b.map(renderRow);
+
+
+let rafID;
+let board = newBoard(true);
+const coords = neighborCoordinatesForBoard(board);
+let neighbors;         // The game board as number of live neighbors per cell.
+
+const main = () => {
+  // Given a board, calculate all the valid neighbor coordinates.
+  neighbors = boardAsNumberOfNeighbors(board, coords);  // Calculate live neighbor counts.
+  board = numberBoardAsLiveDeadCells(neighbors, board); // Calculate next state of board.
+  renderBoard(board);
+  // console.log(board);
+  
+  
+  // if (y == 42) {
+    // cancelAnimationFrame(rafID);
+  // } else {
+  setTimeout(() => {
+    rafID = requestAnimationFrame(main);    
+  }, 100);
+
+  // }
 }
 
-rafID = requestAnimationFrame(animate);
+rafID = requestAnimationFrame(main);
